70ec50c2fe69eecf30dc4383fbf0b824a332fb33
Suhan Wijaya <suhanw@gmail.com>
2021-03-11 15:39:48 -0500

    inline style

--- remote/client/components/top-nav/index.js
+++ remote/client/components/top-nav/index.js
@@ -4,7 +4,8 @@ import style from './style';
 const TopNav = () => {
 
 	return (
-		<nav className={style.topNav}>
+		<nav style={{ border: '2px solid blue', color: 'blue', backgroundColor: '#abe7ff' }}>
+		{/* <nav className={style.topNav}> */}
 			<h3>This is TopNav</h3>
 		</nav>
 	);
